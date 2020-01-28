14b1c0e7b28577cb202c499501963f274ae8e1de
rooneymcnibnug <Jakea350@protonmail.com>
2020-01-27 21:43:46 -0600

    mat2 > mat, & cleaned code

--- mat_clean_dir.sh
+++ mat_clean_dir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-### a dirty .sh to wipe the metadata from all files in an entire directory. The Metadata Anonymisation Toolkit (https://mat.boum.org/)
+### a noob .sh to wipe the metadata from all files in an entire directory. The Metadata Anonymisation Toolkit (https://mat.boum.org/)
 ### is required in order to run this script ($ apt-get install mat). Just plop this .sh in the dir of files you want metadata wiped from.
 
 GREEN='\033[0;32m'
@@ -9,6 +9,10 @@ OLDIFS=$IFS
 IFS=$(echo -en "\n\b")
 for dir in $(find . -type d)
 do
-        (echo -e "${GREEN}~ Displaying metadata found ~${NO_COLOR}" ; mat -d $dir ; echo -e "${GREEN}~ Running Metadata Anonymization tool ~${NO_COLOR}" ; mat $dir ; e$
+        (echo -e "${GREEN}~ Displaying metadata found ~${NO_COLOR}" ;
+        mat2 -d $dir ;
+        echo -e "${GREEN}~ Running Metadata Anonymization tool ~${NO_COLOR}" ;
+        mat2 $dir ;
+        e$
 done
 IFS=$OLDIFS
