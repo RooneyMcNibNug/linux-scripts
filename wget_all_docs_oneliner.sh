ab479c8214abdb94702595977ebc4de76c0a61ef
JA <Jakea350@protonmail.com>
2020-05-01 12:30:28 -0500

    Update wget_all_docs_oneliner.sh

--- wget_all_docs_oneliner.sh
+++ wget_all_docs_oneliner.sh
@@ -1,2 +1,2 @@
-#wget --no-directories --content-disposition -e robots=off -r -R html,htm,jpg,jpeg,gif,png,php,asp,jsp,js,py,css \
+#wget --no-directories --content-disposition -e robots=off -r html,htm,jpg,jpeg,gif,png,php,asp,jsp,js,py,css \
 wget --no-clobber --convert-links --random-wait -r -p -E -e robots=off
