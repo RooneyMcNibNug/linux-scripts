19cc0b9c4e1cf34e410d925cc6b422128c65e657
JA <Jakea350@protonmail.com>
2020-06-08 09:03:50 -0500

    directory creation by date script

---
+++ dir_by_date.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for each in *.mkv
+do
+  date=$(date +%Y-%d-%m -r "$each");
+  _DATES+=($date);
+  FILES+=($each);
+done
+
+DATES=$(printf "%s\n" "${_DATES[@]}" | sort -u);
+for date in ${DATES[@]}; do
+  if [ ! -d "$date" ]; then
+    mkdir "$date"
+  fi
+done
+
+for i in  ${FILES[@]}; do
+  dest=$(date +%Y-%d-%m -r "$i")
+  mv $i $dest/$i
+done
