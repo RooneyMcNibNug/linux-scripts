8cf0b5eedc208f59778a19ea656349851162656d
JA <Jakea350@protonmail.com>
2021-03-05 19:50:12 -0600

    Delete wget_all_docs_oneliner.sh

--- wget_all_docs_oneliner.sh
+++
@@ -1,2 +0,0 @@
-#wget --no-directories --content-disposition -e robots=off -r html,htm,jpg,jpeg,gif,png,php,asp,jsp,js,py,css \
-wget --no-clobber --convert-links --random-wait -r -p -E -e robots=off
