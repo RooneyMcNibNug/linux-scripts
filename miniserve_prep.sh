dbeb70c1f983e7d72c44c915750f62addf76cca0
JA <Jakea350@protonmail.com>
2022-08-12 15:05:02 -0500

    auto-rust installation

--- miniserve_prep.sh
+++ miniserve_prep.sh
@@ -3,9 +3,13 @@
 ## Setup for running a miniserve instance- a very and quick way of setting up an HTTP server - for home 
 ## or organization use in order to share files from a particular machine, server, or VMs directories.
 ## See more about miniserve here: https://github.com/svenstaro/miniserve
-## Here miniserve is installed via Rust, a required dependency: https://www.rust-lang.org/tools/install
 
 ## Please run this as root!
+
+# Here miniserve is installed via Rust, so we will automatically install rust as a required 
+# dependency: https://www.rust-lang.org/tools/install
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
 
 # Install dependencies for miniserve crate
 dnf install make automake gcc gcc-c++ kernel-devel -y
