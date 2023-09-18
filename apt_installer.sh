e4e9200f9e077bb4791b97d3c31c42dc3f8fd933
JA <Jakea350@protonmail.com>
2023-09-18 14:21:09 -0500

    get Siggy

--- apt_installer.sh
+++ apt_installer.sh
@@ -106,6 +106,25 @@ pip3 install magic-wormhole
 
 echo ""
 echo "~ Finished installing. ~"
+echo "~ Installing Signal Desktop.. ~"
+echo ""
+
+# NOTE: These instructions only work for 64-bit Debian-based
+# Linux distributions such as Ubuntu, Mint etc.
+
+# 1. Install our official public software signing key:
+wget2 -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+# 2. Add our repository to your list of repositories:
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  tee /etc/apt/sources.list.d/signal-xenial.list
+
+# 3. Update your package database and install Signal:
+apt update -y && apt install -y signal-desktop
+
+echo ""
+echo "~ Finished installing. ~"
 echo "~ Installing dbeaver.. ~"
 echo ""
 
