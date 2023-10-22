1ecf08476f08a2cec0895ebab68a0e84bcf2793d
JA <Jakea350@protonmail.com>
2023-10-21 20:07:56 -0500

    flatpak install and namechange for script

--- fedora_desktop_installer.sh
+++ fedora_desktop_installer.sh
@@ -122,6 +122,7 @@ flatpak install -y flathub com.makemkv.MakeMKV
 flatpak install -y org.tenacityaudio.Tenacity
 flatpak install -y net.mullvad.MullvadBrowser
 flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub io.github.flattool.Warehouse
 
 echo ""
 echo "~ Finished installing. ~"
