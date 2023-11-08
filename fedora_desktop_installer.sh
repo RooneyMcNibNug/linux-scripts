481ec6760f8573da8c1fe9d1d0906e15ba663cb0
JA <Jakea350@protonmail.com>
2023-11-08 11:04:53 -0600

    Update fedora_desktop_installer.sh

--- fedora_desktop_installer.sh
+++ fedora_desktop_installer.sh
@@ -123,7 +123,7 @@ flatpak install -y org.tenacityaudio.Tenacity
 flatpak install -y net.mullvad.MullvadBrowser
 flatpak install -y flathub com.discordapp.Discord
 flatpak install -y flathub io.github.seadve.Kooha
-flatpak install -y flathub org.kde.arianna
+flatpak install -y flathub com.github.babluboy.bookworm
 
 echo ""
 echo "~ Finished installing. ~"
