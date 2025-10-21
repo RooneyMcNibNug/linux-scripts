81ac2bdfc205f63116e190da3f8cc2f9ba001bee
JA <Jakea350@protonmail.com>
2025-10-21 01:32:06 +0000

    Create jellyfin.bkp.sh

---
+++ jellyfin.bkp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TIME=$(date +%Y%m%d%H%M%S)
+VER=$(jellyfin --version 2>&1 | awk '// {print $NF}')
+
+systemctl stop jellyfin
+
+# Back up the needed dirs
+mkdir -p /media/backups/jellyfin.${TIME}_${VER}
+cp -a /var/lib/jellyfin /media/backups/jellyfin.${TIME}_${VER}/data
+cp -a /etc/jellyfin /media/backups/jellyfin.${TIME}_${VER}/config
+
+# Optional compression of the backups for offsite
+#tar -czf "/media/backups/jellyfin.${TIME}_${VER}.tar.gz" -C /media/backups jellyfin.${TIME}_${VER}
+
+systemctl start jellyfin
